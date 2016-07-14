df0a3a10efd838881ccaadbaedd0a3f0d10b9767
yons <yons@yonsdeiMac.local>
2016-07-14 16:58:44 +0000

    Release 1.0.0.

---
+++ HelloWord/sfsdfgsd.h
@@ -0,0 +1,13 @@
+//
+//  sfsdfgsd.h
+//  copodTe
+//
+//  Created by yons on 16/7/13.
+//  Copyright © 2016年 yons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sfsdfgsd : UIView
+
+@end
---
+++ HelloWord/sfsdfgsd.m
@@ -0,0 +1,21 @@
+//
+//  sfsdfgsd.m
+//  copodTe
+//
+//  Created by yons on 16/7/13.
+//  Copyright © 2016年 yons. All rights reserved.
+//
+
+#import "sfsdfgsd.h"
+
+@implementation sfsdfgsd
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
